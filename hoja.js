7cb393a44684cfa179e3ee8bbaa30dda0b45c885
Nicolas Rovetta <nico.rovetta@gmail.com>
2023-01-20 22:23:28 -0300

    first commit

---
+++ hoja.js
@@ -0,0 +1,15 @@
+
+function miFuncion() {
+    document.getElementById("demo").innerHTML = "Marshall Rodriguez<br>Argentino<br>32 años<br>soltero";
+}
+
+function miFuncion2() {
+    document.getElementById("profesion").innerHTML = "Programador full stack jr con<br>4 años de experiencia,desde hace<br>3 años me especializo en desarrollo front-end con tecnologias:HTML,CSS y JS";
+}
+
+function miFuncion3() {
+    alert("disponible entre las 8:00hs a 20:00hs");
+}
+
+
+
